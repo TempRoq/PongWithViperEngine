3dcf5a6f1508747092fb9c303cceaef7c80b65e7
SkipperKippz456 <84921476+SkipperKippz456@users.noreply.github.com>
2022-04-17 00:06:21 -0400

    win64

--- build/setup_win64.bat
+++ build/setup_win64.bat
@@ -1,15 +1,6 @@
 @echo off
 
-if "%1"=="" goto usage
-
-mkdir %1-win64
-cd %1-win64
-cmake -G "Visual Studio 15 2017 Win64" ../../src/%1
+mkdir pongViper-win64
+cd pongViper-win64
+cmake -G "Visual Studio 16 2019" ../../src/ga1-core
 cd ..
-goto end
-
-:usage
-echo setup_win64.bat [homework_name]
-echo Where homework_name is ga1-core, etc.
-
-:end
